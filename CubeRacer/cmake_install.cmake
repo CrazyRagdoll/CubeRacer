0af98b3e6b440f4c256fee6664d18abe629d7179
Sam Bowen <Samuel_bowen_1994@hotmail.co.uk>
2014-05-01 09:42:21 +0100

    added travis to my computers git repo

--- CubeRacer/cmake_install.cmake
+++ CubeRacer/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/sam/CubeRacer
+# Install script for directory: /home/sam/CubeRacer/CubeRacer
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/sam/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/sam/CubeRacer/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/sam/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/sam/CubeRacer/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
